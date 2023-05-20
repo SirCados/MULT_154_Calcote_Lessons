5efd11401b40d523c127d984fedebd21232952c6
Cade Calcote <cade.m.calcote@gmail.com>
2023-05-20 12:34:41 -0700

    Swarm Patrols, some setup for steering

---
+++ Assets/Scripts/Forest/SwarmScript.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SwarmScript : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _waypoints;
+    [SerializeField] NavMeshAgent _agent;
+    const float WAYPOINT_THRESHOLD = 10.0f;
+    GameObject _currentWaypoint;
+    int _waypointIndex = 0;
+
+    private void Start()
+    {
+        _currentWaypoint = _waypoints[0];
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        MoveToWaypoint();
+    }
+
+    void MoveToWaypoint()
+    {        
+        if(Vector3.Distance(transform.position, _currentWaypoint.transform.position) < WAYPOINT_THRESHOLD)
+        {
+            _currentWaypoint = GetNextWaypoint();
+            _agent.SetDestination(_currentWaypoint.transform.position);
+        }        
+    }
+
+    GameObject GetNextWaypoint()
+    {
+        _waypointIndex++;
+        if (_waypointIndex == _waypoints.Count)
+        {
+            _waypointIndex = 0;
+        }
+
+        return _waypoints[_waypointIndex];
+    }
+}
