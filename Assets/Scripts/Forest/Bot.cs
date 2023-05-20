1117703948c5994efc521f0d23992bec4bcc647a
Cade Calcote <cade.m.calcote@gmail.com>
2023-05-20 13:20:36 -0700

    Steering Behavior Switch
    Added a switch containing all steering behaviors, confirmed that Evade, Flee, Pursue, and Seek work.

--- Assets/Scripts/Forest/Bot.cs
+++ Assets/Scripts/Forest/Bot.cs
@@ -2,62 +2,108 @@
 /// Code modified from https://learn.unity.com/tutorial/hide-h1zl/?courseId=5dd851beedbc2a1bf7b72bed&projectId=5e0b9220edbc2a14eb8c9356&tab=materials&uv=2019.3#
 /// Author: Penny de Byl
 ///
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Bot : MonoBehaviour
 {
-    NavMeshAgent agent;
-    public GameObject target;
-    public GameObject[] hidingSpots;
-
-    float currentSpeed
+    public enum BehaviorMode
     {
-        get { return agent.velocity.magnitude; }
+        EVADE,
+        FLEE,
+        HIDE,
+        PURSUE,
+        SEEK,
+        WANDER
     }
+
+    public GameObject Target;
+    public GameObject[] HidingSpots;
+    public BehaviorMode CurrentBehavior;
+
+    NavMeshAgent _agent;
+    Rigidbody _targetRigidbody;
+
+    float _targetSpeed
+    {
+        get { return _targetRigidbody.velocity.magnitude; }
+    }    
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = this.GetComponent<NavMeshAgent>();
+        _agent = this.GetComponent<NavMeshAgent>();
+        _targetRigidbody = Target.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(CanTargetSeeMe())
+        //Flee(Target.transform.position);
+        DetermineBehavior();
+    }
+
+    void DetermineBehavior()
+    {
+        switch (CurrentBehavior)
+        {
+            case BehaviorMode.EVADE:
+                Evade();
+                break;
+            case BehaviorMode.FLEE:
+                Flee(Target.transform.position);
+                break;
+            case BehaviorMode.HIDE:
+                Hide();
+                break;
+            case BehaviorMode.PURSUE:
+                Pursue();
+                break;
+            case BehaviorMode.SEEK:
+                Seek(Target.transform.position);
+                break;
+            case BehaviorMode.WANDER:
+                Wander();
+                break;
+        }
     }
 
     void Seek(Vector3 location)
     {
-        agent.SetDestination(location);
+        _agent.SetDestination(location);
+        _targetRigidbody = Target.GetComponent<Rigidbody>();
     }
 
     void Flee(Vector3 location)
     {
         Vector3 fleeVector = location - this.transform.position;
-        agent.SetDestination(this.transform.position - fleeVector);
+        _agent.SetDestination(this.transform.position - fleeVector);
     }
 
     void Pursue()
     {
-        Vector3 targetDir = target.transform.position - this.transform.position;
+        Vector3 targetDir = Target.transform.position - this.transform.position;
 
-        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(Target.transform.forward));
 
         float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
 
-        if ((toTarget > 90 && relativeHeading < 20) || currentSpeed < 0.01f)
+        if ((toTarget > 90 && relativeHeading < 20) || _targetSpeed < 0.01f)
         {
-            Seek(target.transform.position);
+            Seek(Target.transform.position);
             return;
         }
 
-        float lookAhead = targetDir.magnitude / (agent.speed + currentSpeed);
-        Seek(target.transform.position + target.transform.forward * lookAhead);
+        float lookAhead = targetDir.magnitude / (_agent.speed + _targetSpeed);
+        Seek(Target.transform.position + Target.transform.forward * lookAhead);
     }
 
     void Evade()
     {
-        Vector3 targetDir = target.transform.position - this.transform.position;
-        float lookAhead = targetDir.magnitude / (agent.speed + currentSpeed);
-        Flee(target.transform.position + target.transform.forward * lookAhead);
+        Vector3 targetDir = Target.transform.position - this.transform.position;
+        float lookAhead = targetDir.magnitude / (_agent.speed + _targetSpeed);
+        Flee(Target.transform.position + Target.transform.forward * lookAhead);
     }
 
 
@@ -85,10 +131,10 @@ public class Bot : MonoBehaviour
         float dist = Mathf.Infinity;
         Vector3 chosenSpot = Vector3.zero;
 
-        for (int i = 0; i < hidingSpots.Length; i++)
+        for (int i = 0; i < HidingSpots.Length; i++)
         {
-            Vector3 hideDir =hidingSpots[i].transform.position - target.transform.position;
-            Vector3 hidePos = hidingSpots[i].transform.position + hideDir.normalized * 10;
+            Vector3 hideDir =HidingSpots[i].transform.position - Target.transform.position;
+            Vector3 hidePos = HidingSpots[i].transform.position + hideDir.normalized * 10;
 
             if (Vector3.Distance(this.transform.position, hidePos) < dist)
             {
@@ -106,18 +152,18 @@ public class Bot : MonoBehaviour
         float dist = Mathf.Infinity;
         Vector3 chosenSpot = Vector3.zero;
         Vector3 chosenDir = Vector3.zero;
-        GameObject chosenGO = hidingSpots[0];
+        GameObject chosenGO = HidingSpots[0];
 
-        for (int i = 0; i < hidingSpots.Length; i++)
+        for (int i = 0; i < HidingSpots.Length; i++)
         {
-            Vector3 hideDir =hidingSpots[i].transform.position - target.transform.position;
-            Vector3 hidePos = hidingSpots[i].transform.position + hideDir.normalized * 100;
+            Vector3 hideDir =HidingSpots[i].transform.position - Target.transform.position;
+            Vector3 hidePos = HidingSpots[i].transform.position + hideDir.normalized * 100;
 
             if (Vector3.Distance(this.transform.position, hidePos) < dist)
             {
                 chosenSpot = hidePos;
                 chosenDir = hideDir;
-                chosenGO = hidingSpots[i];
+                chosenGO = HidingSpots[i];
                 dist = Vector3.Distance(this.transform.position, hidePos);
             }
         }
@@ -136,7 +182,7 @@ public class Bot : MonoBehaviour
     bool CanSeeTarget()
     {
         RaycastHit raycastInfo;
-        Vector3 rayToTarget = target.transform.position - this.transform.position;
+        Vector3 rayToTarget = Target.transform.position - this.transform.position;
         if (Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
         {
             if (raycastInfo.transform.gameObject.tag == "Player")
@@ -148,21 +194,14 @@ public class Bot : MonoBehaviour
     bool CanTargetSeeMe()
     {
         RaycastHit raycastInfo;
-        Vector3 targetFwdWS = target.transform.TransformDirection(target.transform.forward);
-        Debug.DrawRay(target.transform.position, targetFwdWS * 10);
-        Debug.DrawRay(target.transform.position, target.transform.forward * 10, Color.green);
-        if (Physics.Raycast(target.transform.position, target.transform.forward, out raycastInfo))
+        Vector3 targetFwdWS = Target.transform.TransformDirection(Target.transform.forward);
+        Debug.DrawRay(Target.transform.position, targetFwdWS * 10);
+        Debug.DrawRay(Target.transform.position, Target.transform.forward * 10, Color.green);
+        if (Physics.Raycast(Target.transform.position, Target.transform.forward, out raycastInfo))
         {
             if (raycastInfo.transform.gameObject == gameObject)
                 return true;
         }
         return false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(CanTargetSeeMe())
-            Seek(target.transform.position);
     }
 }
